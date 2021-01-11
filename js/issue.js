a2e0eeeab8fee2ea71d757c2d2416c78603752de
kecsou <kevin.voyer.developpeur@gmail.com>
2021-01-11 11:59:11 +0100

    handle promise error get key infos

--- js/issue.js
+++ js/issue.js
@@ -38,118 +38,127 @@ function catchingCallback(error) {
   return null;
 }
 
+let attempt = 0;
 export async function init() {
-  const { id } = await launcher.getkeyinfo('tracker');
+  try {
+    const { id } = await launcher.getkeyinfo('tracker');
 
-  if (id) {
-    $('#top-issue-container').css('display', 'block');
-    $('#top-issue')
-      .click(async () => {
-        const titleWindowBug = languages.getTranslate('');
-        const cancelButton = languages.getTranslate('');
-        const sendButton = languages.getTranslate('');
+    if (id) {
+      $('#top-issue-container').css('display', 'block');
+      $('#top-issue')
+        .click(async () => {
+          const titleWindowBug = languages.getTranslate('');
+          const cancelButton = languages.getTranslate('');
+          const sendButton = languages.getTranslate('');
 
-        const {
-          cn,
-          givenName,
-          mail,
-          provider,
-          providertype,
-          pulseaudiotcpport,
-          sessionid,
-          target_ip: targetIp,
-        } = window.od.currentUser;
+          const {
+            cn,
+            givenName,
+            mail,
+            provider,
+            providertype,
+            pulseaudiotcpport,
+            sessionid,
+            target_ip: targetIp,
+          } = window.od.currentUser;
 
-        const awaitingSpawnerVersion = launcher.getSpawnerVersion()
-          .catch(catchingCallback);
+          const awaitingSpawnerVersion = launcher.getSpawnerVersion()
+            .catch(catchingCallback);
 
-        const awaitingPyosVersion = launcher.getPyosVersion()
-          .catch(catchingCallback);
+          const awaitingPyosVersion = launcher.getPyosVersion()
+            .catch(catchingCallback);
 
-        const awaitingWebModulesVersion = launcher.getWebModulesVersion()
-          .catch(catchingCallback);
+          const awaitingWebModulesVersion = launcher.getWebModulesVersion()
+            .catch(catchingCallback);
 
-        const awaitingUserContainerInfos = launcher.about()
-          .catch(catchingCallback);
+          const awaitingUserContainerInfos = launcher.about()
+            .catch(catchingCallback);
 
-        const [
-          spawnerVersion,
-          pyosVersion,
-          webModulesVersion,
-          userContainerInfos,
-        ] = await Promise.all([
-          awaitingSpawnerVersion,
-          awaitingPyosVersion,
-          awaitingWebModulesVersion,
-          awaitingUserContainerInfos,
-        ]);
+          const [
+            spawnerVersion,
+            pyosVersion,
+            webModulesVersion,
+            userContainerInfos,
+          ] = await Promise.all([
+            awaitingSpawnerVersion,
+            awaitingPyosVersion,
+            awaitingWebModulesVersion,
+            awaitingUserContainerInfos,
+          ]);
 
-        const issue = {
-          issue: {
-            name: 'Bug',
-          },
-          summary: '',
-          description: '',
-          cn,
-          givenName,
-          mail,
-          provider,
-          providertype,
-          pulseaudiotcpport,
-          sessionid,
-          screen,
-          targetIp,
-          os: whichBrowser.getOSInfo()?.name,
-          browser: whichBrowser.getBrowserInfo()?.name,
-          spawnerVersion,
-          pyosVersion,
-          webModulesVersion,
-          ...userContainerInfos,
-        };
-
-        bootbox.dialog({
-          title: titleWindowBug || 'Issue',
-          message: `
-            <h3 class="text-center">Sumbit a new issue</h3>
-            <div class="container w-100 h-75 d-flex justify-content-center flex-column">  
-              <input id="issue-summary" placeholder="Summary" />
-              <textarea id="issue-description" class="h-50" placeholder="Your report"></textarea>
-            </div>
-          `,
-          className: 'window-dialog',
-          onEscape: true,
-          backdrop: true,
-          buttons: {
-            cancel: {
-              label: cancelButton || 'Cancel',
+          const issue = {
+            issue: {
+              name: 'Bug',
             },
-            send: {
-              label: sendButton || 'Send',
-              className: 'window-button',
-              callback: () => {
-                postIssue(issue)
-                  .then(() => {
-                    notificationSystem.displayNotification('Issue', 'Your issue hase been posted', 'info');
-                  })
-                  .catch((error) => {
-                    notificationSystem.displayNotification('Issue', error, 'error');
-                    console.error(error);
-                  });
+            summary: '',
+            description: '',
+            cn,
+            givenName,
+            mail,
+            provider,
+            providertype,
+            pulseaudiotcpport,
+            sessionid,
+            screen,
+            targetIp,
+            os: whichBrowser.getOSInfo()?.name,
+            browser: whichBrowser.getBrowserInfo()?.name,
+            spawnerVersion,
+            pyosVersion,
+            webModulesVersion,
+            ...userContainerInfos,
+          };
+
+          bootbox.dialog({
+            title: titleWindowBug || 'Issue',
+            message: `
+              <h3 class="text-center">Sumbit a new issue</h3>
+              <div class="container w-100 h-75 d-flex justify-content-center flex-column">  
+                <input id="issue-summary" placeholder="Summary" />
+                <textarea id="issue-description" class="h-50" placeholder="Your report"></textarea>
+              </div>
+            `,
+            className: 'window-dialog',
+            onEscape: true,
+            backdrop: true,
+            buttons: {
+              cancel: {
+                label: cancelButton || 'Cancel',
+              },
+              send: {
+                label: sendButton || 'Send',
+                className: 'window-button',
+                callback: () => {
+                  postIssue(issue)
+                    .then(() => {
+                      notificationSystem.displayNotification('Issue', 'Your issue hase been posted', 'info');
+                    })
+                    .catch((error) => {
+                      notificationSystem.displayNotification('Issue', error, 'error');
+                      console.error(error);
+                    });
+                },
               },
             },
-          },
-          animate: false,
+            animate: false,
+          });
+
+          $('#issue-summary')
+            .on('input', function () {
+              issue.summary = this.value;
+            });
+
+          $('#issue-description')
+            .on('input', function () {
+              issue.description = this.value;
+            });
         });
-
-        $('#issue-summary')
-          .on('input', function () {
-            issue.summary = this.value;
-          });
-
-        $('#issue-description')
-          .on('input', function () {
-            issue.description = this.value;
-          });
-      });
+    }
+  } catch (e) {
+    console.error(e);
+    attempt++;
+    if (attempt <= 5) {
+      await init();
+    }
   }
 }
