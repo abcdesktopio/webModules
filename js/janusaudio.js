3e0b451e87222a03ce6f39576a17269a49ebe698
kecsou <kevin.voyer.developpeur@gmail.com>
2021-01-22 22:51:38 +0100

    janus: v1

--- js/janusaudio.js
+++ js/janusaudio.js
@@ -11,221 +11,232 @@
 * Software description: cloud native desktop service
 */
 
-/**
- * @typedef getStream
- * @property {Number} id
- * @property {String} name
- * @property {String} description
- * @property {String} pin
- * @property {Boolean} enabled
- * @property {Boolean} audio
- * @property {Number} audiopt
- * @property {String} audiortpmap
- * @property {String} live
- * @property {Number} audioport
- * @property {Number} audiortcpport
- * @property {Number} audio_age_ms
- * @property {String} host
- */
+import * as launcher from './launcher.js';
 
-// import * as launcher from './launcher.js';
+class JanusAbcDesktop extends Janus {
+  static init() {
+    return new Promise((resolve) => {
+      super.init({
+        debug: 'all',
+        dependencies: super.useDefaultDependencies(),
+        callback: () => {
+          resolve();
+        },
+      });
+    });
+  }
 
-const server = '';
-const opaqueId = `streamingtest-${Janus.randomString(12)}`;
+  static destroyCurrentSession() {
+    if (JanusAbcDesktop.currentJanusSession) {
+      JanusAbcDesktop.currentJanusSession.destroy();
+      JanusAbcDesktop.currentJanusSession = null;
+    }
+  }
 
-let currentJanusSession = null;
-
-/**
- * @type {getStream}
- * @desc Store the getStream object provided by pyos /API/webrtc/getStream
- * It initialised on broadway.connected
- */
-let getStream = null;
-
-Janus.prototype.abcdesktopAttached = false;
-Janus.prototype.abcdesktopStreaming = null;
-
-/**
- * @param {HTMLElement} eltStream
- * @desc Custome function which allow to attach the opened session with a plugin
- * Resolve when the remote provide a stream and it has been attached to the provided html element.
- * The promise is rejected on attachement error
- */
-Janus.prototype.abcdesktopAttach = function (eltStream = null) {
-  const janusSession = this;
-
-  if (!janusSession.abcdesktopAttached) {
-    janusSession.abcdesktopAttached = true;
+  /**
+  * @desc Allow to get a new Janus session
+  */
+  static createSession(server, pin) {
     return new Promise((resolve, reject) => {
-      const optionsAttach = {
-        plugin: 'janus.plugin.streaming',
-        opaqueId,
-        success: successAttach,
-        error: errorAttach,
-        onmessage: onmessageAttach,
-        onremotestream: onremotestreamAttach,
-        oncleanup() {
-
-        }, // Don't used but I keep it there to know it is possible to add an action at this moment
+      const optionsJanus = {
+        server,
+        pin,
+        success: successSession,
+        error: errorSession,
       };
 
-      janusSession.attach(optionsAttach);
+      const janusSession = new JanusAbcDesktop(optionsJanus); // This is a Janus session
 
       /**
-             *
-             * @param {*} streaming
-             * @desc
-             */
-      function successAttach(streaming) {
-        const body = { request: 'watch', id: 4 };
-        console.log(streaming);
-        janusSession.abcdesktopStreaming = streaming;
-        streaming.send({ message: body });
+       * @desc Resolve the promise with the janussSession when it has been open
+       */
+      function successSession() {
+        resolve(janusSession);
       }
 
       /**
-             *
-             * @param {*} error
-             * @desc
-             */
-      function errorAttach(error) {
-        janusSession.abcdesktopAttached = false;
-        Janus.error('  -- Error attaching plugin... ', error);
-        reject();
-      }
-
-      /**
-             *
-             * @param {*} msg
-             * @param {*} jsep
-             * @desc
-             */
-      function onmessageAttach(msg, jsep) {
-        Janus.debug(' ::: Got a message :::');
-        Janus.debug(msg);
-        if (jsep !== undefined && jsep !== null) {
-          Janus.debug('Handling SDP as well...');
-          Janus.debug(jsep);
-          const answer = {
-            jsep,
-            media: { audioSend: false, videoSend: false },
-            // We want recvonly audio/video
-            success(jsep) {
-              Janus.debug('Got SDP!');
-              Janus.debug(jsep);
-              const body = { request: 'start' };
-              janusSession.abcdesktopStreaming.send({ message: body, jsep });
-            },
-            error(error) {
-              Janus.error('WebRTC error:', error);
-            },
-          };
-
-          janusSession.abcdesktopStreaming.createAnswer(answer);
-        }
-      }
-
-      /**
-             *
-             * @param {*} stream
-             * @desc
-             */
-      function onremotestreamAttach(stream) {
-        try {
-          Janus.attachMediaStream(eltStream, stream);
-          resolve();
-        } catch (e) {
-          reject(e);
-        }
+       *
+       * @desc Call on error from the session
+       */
+      function errorSession(error) {
+        JanusAbcDesktop.error(error);
+        reject(error);
       }
     });
   }
 
-  return Promise.resolve();
-};
+  constructor(optionsJanus) {
+    super(optionsJanus);
+    this.attached = false;
+    this.streaming = null;
+  }
+
+  /**
+   * @param {HTMLElement} eltStream
+   * @desc Custome function which allow to attach the opened session with a plugin
+   * Resolve when the remote provide a stream and it has been attached to the provided html element.
+   * The promise is rejected on attachement error
+   */
+  attachElt(eltStream = null) {
+    if (!this.attached) {
+      this.attached = true;
+      return new Promise((resolve, reject) => {
+        const optionsAttach = {
+          plugin: 'janus.plugin.streaming',
+          opaqueId: JanusAbcDesktop.opaqueId,
+          success: (streaming) => {
+            console.log(streaming);
+            this.streaming = streaming;
+            resolve();
+          },
+          error: (error) => {
+            this.attached = false;
+            JanusAbcDesktop.error('  -- Error attaching plugin... ', error);
+            reject();
+          },
+          onmessage: (msg, jsep) => {
+            JanusAbcDesktop.debug(' ::: Got a message :::');
+            JanusAbcDesktop.debug(msg);
+            if (jsep !== undefined && jsep !== null) {
+              JanusAbcDesktop.debug('Handling SDP as well...');
+              JanusAbcDesktop.debug(jsep);
+              const answer = {
+                jsep,
+                media: { audioSend: false, videoSend: false },
+                // We want recvonly audio/video
+                success: (jsep) => {
+                  JanusAbcDesktop.debug('Got SDP!');
+                  JanusAbcDesktop.debug(jsep);
+                  const body = { request: 'start' };
+                  this.streaming.send({ message: body, jsep });
+                },
+                error: (error) => {
+                  JanusAbcDesktop.error('WebRTC error:', error);
+                },
+              };
+
+              this.streaming.createAnswer(answer);
+            }
+          },
+          onremotestream: (stream) => {
+            JanusAbcDesktop.attachMediaStream(eltStream, stream);
+          },
+          ondata: (data) => {
+            JanusAbcDesktop.debug('We got data from the DataChannel!', data);
+          },
+          oncleanup() {},
+          // Don't used but I keep it there to know it is possible to add an action at this moment
+        };
+
+        this.attach(optionsAttach);
+      });
+    }
+
+    return Promise.resolve();
+  }
+
+  /**
+   * @desc Allow  to start the stream
+   */
+  async startStream(id) {
+    const body = { request: 'watch', id };
+    this.streaming.send({ message: body });
+  }
+
+  /**
+   * @desc Allow to stop the stream
+   */
+  stopStream() {
+    const body = { request: 'stop' };
+    this.streaming.send({ message: body });
+  }
+
+  getStreamList() {
+    return new Promise((resolve, reject) => {
+      if (this.streaming !== null) {
+        const body = { request: 'list' };
+        this.streaming.send({
+          message: body,
+          success: (result) => {
+            if (result.list instanceof Array) {
+              resolve(result.list);
+            } else {
+              reject(result);
+            }
+          },
+          error: (e) => {
+            reject(e);
+          },
+        });
+      } else {
+        reject();
+      }
+    });
+  }
+}
+JanusAbcDesktop.currentJanusSession = null;
+JanusAbcDesktop.opaqueId = `streamingtest-${JanusAbcDesktop.randomString(12)}`;
+
+window.JanusAbcDesktop = JanusAbcDesktop;
 
 /**
  * @desc Create a stream element wich will be bind with remote's stream afteward.
  * Initialise Janus
  */
 const init = async () => {
-  const video = document.createElement('video');
-  video.id = 'janusStream';
-  video.style.display = 'none';
+  const audio = document.createElement('audio');
+  audio.id = 'janusStream';
+  audio.autoplay = true;
+  audio.style = 'width: 100%; height: 100%;';
 
-  document.body.appendChild(video);
+  document.body.appendChild(audio);
 
   /**
    * @desc
    * The session will be create when the client select the audio button
    */
-  const initOptions = { debug: 'all' };
+  await JanusAbcDesktop.init();
 
-  Janus.init(initOptions);
   try {
-    const { result } = await launcher.getStream();
-    getStream = result;
-    const { host, audioport } = getStream;
-    await launcher.configurePulse(host, audioport);
+    const {
+      result: {
+        id,
+        host,
+        hostip,
+        audioport,
+        pin,
+      },
+    } = await launcher.getStream();
+
+    await launcher.configurePulse(hostip, audioport);
+    const janusSession = await JanusAbcDesktop.createSession(`https://${host}/janus`, pin);
+    await janusSession.attachElt(audio, pin);
+    await janusSession.startStream(id);
   } catch (e) {
     console.error(e);
   }
 };
 
 /**
- * @desc Allow to get a new Janus session
- */
-const createSession = () => new Promise((resolve, reject) => {
-  const optionsJanus = {
-    server, // `http://${host}:8088/janus`
-    success: successSession,
-    error: errorSession,
-  };
-
-  const janusSession = new Janus(optionsJanus); // This is a Janus session
-
-  /**
-   * @desc Resolve the promise with the janussSession when it has been open
-   */
-  function successSession() {
-    // Attach to streaming plugin
-    resolve(janusSession);
-  }
-
-  /**
-   *
-   * @desc Call on error from the session
-   */
-  function errorSession(error) {
-    Janus.error(error);
-    reject(error);
-  }
-});
-
-/**
  * @desc return a promise wich resolve a januse session connected
  */
 export const openSession = () => {
-  if (!currentJanusSession) {
-    return createSession();
+  if (!JanusAbcDesktop.currentJanusSession) {
+    return JanusAbcDesktop.createSession();
   }
 
-  return Promise.resolve(currentJanusSession);
+  return Promise.resolve(JanusAbcDesktop.currentJanusSession);
 };
 
 /**
  * @desc Destroy the current janusSession
  */
 export const destroySession = () => {
-  if (currentJanusSession) {
-    currentJanusSession.destroy();
-    currentJanusSession = null;
-  }
+  JanusAbcDesktop.destroyCurrentSession();
 };
 
-export const janusSupported = () => Janus.isWebrtcSupported();
+export const janusSupported = () => JanusAbcDesktop.isWebrtcSupported();
 
-/*
-document.addEventListener("broadway.connected", () => {
-    init();
-}); */
+document.addEventListener('broadway.connected', () => {
+  init();
+});
