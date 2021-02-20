e01c51a2c54606fa9c63a1496e0880c924c67d65
kecsou <kevin.voyer.developpeur@gmail.com>
2021-02-20 13:47:21 +0100

    feat: create callTransformAndCopyJSFile function

--- transpile/utils.js
+++ transpile/utils.js
@@ -3,6 +3,7 @@ import path from 'path';
 
 const dirname = path.resolve();
 const pathWorker = path.join(dirname, 'replaceWorker.js');
+const pathWorkerTransformAndCopyJSFile = path.join(dirname, 'transformAndCopyJSFile.js');
 
 /**
  *
@@ -20,6 +21,32 @@ export function callReplaceWorker(filename, searchValue, replaceValue) {
       };
 
       const worker = new Worker(pathWorker, { workerData });
+
+      worker.on('exit', () => {
+        resolve();
+      });
+
+      worker.on('error', (err) => {
+        console.error(err);
+        reject(err);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+/**
+ * 
+ * @param {string} legacyPath 
+ * @param {Object} opts 
+ * @param {string} filename 
+ */
+export function callTransformAndCopyJSFile(legacyPath, opts, filename) {
+  return new Promise((resolve, reject) => {
+    try {
+      const workerData = { legacyPath, opts, filename };
+      const worker = new Worker(pathWorkerTransformAndCopyJSFile, { workerData });
 
       worker.on('exit', () => {
         resolve();
