bcef0f8fef7bf1164559e87fb95e78415cedbec0
kecsou <kevin.voyer.developpeur@gmail.com>
2021-02-22 11:51:35 +0100

    fix: check if the svg file includes the search  value instead of the replace one

--- transpile/replaceWorker.js
+++ transpile/replaceWorker.js
@@ -24,7 +24,7 @@ import fs from 'fs';
 async function bootstrap() {
   const { filename, searchValue, replaceValue } = workerData;
   const svgContent = await fs.promises.readFile(filename, 'utf8');
-  if (svgContent.includes(replaceValue)) {
+  if (svgContent.includes(searchValue)) {
     const newContent = svgContent.replace(new RegExp(searchValue, 'g'), replaceValue);
     await fs.promises.writeFile(filename, newContent);
   }
