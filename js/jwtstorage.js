87358c8266b288f95da4e07061c36ae43c03aec8
kecsou <kevin.voyer.developpeur@gmail.com>
2021-05-20 12:03:00 +0200

    feat: create jwtstorage

---
+++ js/jwtstorage.js
@@ -0,0 +1,16 @@
+/*
+* Software Name : abcdesktop.io
+* Version: 0.2
+* SPDX-FileCopyrightText: Copyright (c) 2020-2021 Orange
+* SPDX-License-Identifier: GPL-2.0-only
+*
+* This software is distributed under the GNU General Public License v2.0 only
+* see the "license.txt" file for more details.
+*
+* Author: abcdesktop.io team
+* Software description: cloud native desktop service
+*/
+
+if (jwt_user_token) {
+  localStorage.setItem('abcdesktop_jwt_user_token', jwt_user_token);
+}
