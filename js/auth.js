180376b7a33703a48de03169e2479b1ddbeeb3de
Matt307082 <matteobeghelli2003@gmail.com>
2025-06-23 11:55:02 +0200

    change providers front generation

--- js/auth.js
+++ js/auth.js
@@ -255,12 +255,49 @@ export class ExplicitAuthManager extends AuthManager {
 
 export class LoginButtonAuthManager extends AuthManager {
   createProvider(config) {
-    let $provider = $(`#login-${config.name}`, this.$ui);
-    if (!$provider.length) {
-      $provider = $('<li></li>').append($('<a></a>').text(`Connect with ${config.displayname || config.name}`));
-      $('ul', this.$ui).first().append($provider);
+    let existing = document.getElementById(`login-${config.name}`);
+    if (!existing) {
+      const li = document.createElement('li');
+      li.id = `login-${config.name}`;
+      li.style.backgroundColor = config.backgroundcolor;
+      li.style.backgroundSize = '30px';
+      li.style.display = 'flex';
+      li.style.alignItems = 'center';
+      li.style.paddingLeft = '10px';
+
+      const a = document.createElement('a');
+      a.id = `connect-with-${config.name}`;
+      a.href = '#';
+      a.style.color = config.textcolor;
+
+      const img = document.createElement('img');
+      img.src = `data:image/svg+xml;base64,${config.icondata}`;
+      img.alt = `${config.name} logo`;
+      img.style.width = '40px';
+      img.style.height = '40px';
+      img.style.marginRight = '12px';
+
+      a.appendChild(document.createTextNode(`Sign-in with ${config.displayname || config.name} account`));
+
+      li.appendChild(img)
+      li.appendChild(a);
+
+      const gpList = document.getElementById('connectGPList');
+      const openidList =document.getElementById('connectOpenidList');
+
+      if(gpList) {
+        gpList.appendChild(li);
+      } 
+
+      if (openidList) {
+        setTimeout(() => {
+          const clone = li.cloneNode(true);
+          openidList.appendChild(clone);
+        }, 0);
+      } 
     }
 
+    let $provider = $(`#login-${config.name}`, this.$ui);
     const self = this;
 
     $('a', $provider)
@@ -270,7 +307,7 @@ export class LoginButtonAuthManager extends AuthManager {
         self.manageLogin($(this).data('auth-provider'));
       });
 
-    $provider.closest('li').show();
+    $provider.show();
 
     return super.createProvider(config);
   }
